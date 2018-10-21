b4e6c468db4737fe5cdeb953125ad838d67cf8b4
AnmoLSin <anisaggu8@gmail.com>
2018-10-21 21:35:00 +0530

    Rotate List Added

---
+++ Linked_Lists/Rotate_List.c
@@ -0,0 +1,44 @@
+// Please visit this link for question
+ListNode* Solution::rotateRight(ListNode* A, int B)
+{
+    ListNode* curr = A;
+    ListNode* head = A;
+    ListNode* last;
+
+    int sizeOv = 0;
+
+    if(B == 0 || A == NULL)
+    {
+        return A;
+    }
+
+    while(curr != NULL)
+    {
+        last = curr;
+        curr = curr->next;
+        sizeOv++;
+    }
+
+    while(B >= sizeOv)
+    {
+        B = B - sizeOv;
+    }
+
+    B = sizeOv - B;
+
+    last->next = head;
+
+    curr = A;
+
+    while(B != 0)
+    {
+        last = curr;
+        curr = curr->next;
+        B--;
+    }
+
+    head = curr;
+    last->next = NULL;
+
+    return head;
+}
